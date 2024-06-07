d59e7aaf5bda1664395e33ef2476d10473f6f187
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-06-07 03:55:18 +0200

    task 7

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_inorder - tranverse in the binary tree using the inorder algo
+ * @tree: The tree
+ * @func: function to print the content of the node
+ * Return: Nothing
+ */
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree && func)
+		if (tree != NULL && func)
+		{
+			binary_tree_preorder(tree->left, func);
+			func(tree->n);
+			binary_tree_preorder(tree->right, func);
+		}
+}
