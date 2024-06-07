1062914203d96946cd23445ba1a76a2636a6cf2d
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-06-07 04:49:02 +0200

    task10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -10,22 +10,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t depth = binary_tree_height(tree);
 
+	if (tree)
+		depth = tree->parent ? binary_tree_height(tree->parent) + 1 : 0;
 	return (depth);
-}
-
-/**
- * binary_tree_height - returns the height of a binary tree
- * @tree: the tree
- * Return: the height
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t left_height, right_height;
-
-	if (tree == NULL)
-		return (0);
-	left_height = tree->left ? binary_tree_height(tree->left) + 1 : 0;
-	right_height = tree->right ? binary_tree_height(tree->right) + 1 : 0;
-
-	return (left_height > right_height ? left_height : right_height);
 }
