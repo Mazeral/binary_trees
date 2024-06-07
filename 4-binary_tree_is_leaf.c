6d86e202267e3526eac5c5ed19ce639f3c2cd88a
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-06-07 03:37:07 +0200

    task 4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -8,7 +8,10 @@
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node->right == NULL && node->left == NULL)
-		return (1);
+	if (node)
+	{
+		if (node->right == NULL && node->left == NULL)
+			return (1);
+	}
 	return (0);
 }
