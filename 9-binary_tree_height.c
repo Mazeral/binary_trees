082ec398c4ca5fed8e177aed777f8723151bea24
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-06-07 04:06:51 +0200

    task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,9 +9,11 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left_height, right_height;
 	if (tree == NULL)
 		return (0);
-	if (tree->left == NULL && tree->right == NULL)
-		return (0);
-	return ( 1 + fmax(binary_tree_height(tree->right), binary_tree_height(tree->left)));
+	left_height = tree->left ? binary_tree_height(tree->left) + 1 : 0;
+	right_height = tree->right ? binary_tree_height(tree->right) + 1 : 0;
+
+	return (left_height > right_height ? left_height : right_height);
 }
