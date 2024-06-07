f26423897ddd6cdb479ba4f83c04facd563d1639
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-06-07 04:01:07 +0200

    task 8

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_postorder - tranverse in the binary tree using the postorder algo
+ * @tree: The tree
+ * @func: function to print the content of the node
+ * Return: Nothing
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree && func)
+		if (tree != NULL && func)
+		{
+			binary_tree_postorder(tree->left, func);
+			binary_tree_postorder(tree->right, func);
+			func(tree->n);
+		}
+}
