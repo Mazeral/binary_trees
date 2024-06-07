04def154ac4b1f39a197482fa2f1fcdebf94641b
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-06-07 05:21:10 +0200

    task14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,6 @@
 #include "binary_trees.h"
 
+size_t binary_tree_size(const binary_tree_t *tree);
 /**
  * binary_tree_balance - calculated the balance factor of the tree
  * @tree: The tree
@@ -17,4 +18,16 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (balance_factor < 0)
 		return (balance_factor * -1);
 	return (balance_factor);
+}
+
+/**
+ * binary_tree_size - Calculated the size of the binary tree
+ * @tree: The tree
+ * Return: The size
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
