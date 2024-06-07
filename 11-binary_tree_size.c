4181b62a195072c393055451a65e6a005e519afd
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-06-07 04:54:57 +0200

    task11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -10,24 +10,5 @@ size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	if (tree->left == NULL && tree->right == NULL)
-		return (0);
-	return ( 1 + binary_tree_height(tree->right) + binary_tree_height(tree->left));
-}
-
-/**
- * binary_tree_height - returns the height of a binary tree
- * @tree: the tree
- * Return: the height
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t left_height, right_height;
-
-	if (tree == NULL)
-		return (0);
-	left_height = tree->left ? binary_tree_height(tree->left) + 1 : 0;
-	right_height = tree->right ? binary_tree_height(tree->right) + 1 : 0;
-
-	return (left_height > right_height ? left_height : right_height);
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
