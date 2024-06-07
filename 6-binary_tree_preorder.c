687ca30d9f535e113b243991a1dab94ca4d02ed1
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-06-07 03:51:39 +0200

    task 6

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,19 +1,19 @@
 #include "binary_trees.h"
 void print_node_val(int);
 /**
- * binary_tree_preorder - a function that tranverse in the binary tree using the pre_order algo
+ * binary_tree_preorder - tranverse in the binary tree using the pre_order algo
  * @tree: The tree
  * @func: function to print the content of the node
- * Return: Nothing*/
+ * Return: Nothing
+ */
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree && func)
-		if(tree != NULL && func)
+		if (tree != NULL && func)
 		{
 			func(tree->n);
 			binary_tree_preorder(tree->left, func);
 			binary_tree_preorder(tree->right, func);
 		}
-	return;
 }
