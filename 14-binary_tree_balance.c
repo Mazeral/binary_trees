4071fe428b49d1953d59299f4be2aa9f079cf412
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-06-08 03:21:57 +0200

    leaves

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,16 +8,7 @@ size_t binary_tree_size(const binary_tree_t *tree);
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	long long int  balance_factor = 0;
-	size_t left = 0;
-	size_t right = 0;
-
-	left = binary_tree_size(tree->left);
-	right = binary_tree_size(tree->right);
-	balance_factor = left - right;
-	if (balance_factor < 0)
-		return (balance_factor * -1);
-	return (balance_factor);
+	return (int)(binary_tree_size(tree->left) - binary_tree_size(tree->right));
 }
 
 /**
@@ -27,7 +18,13 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
+	size_t left_height, right_height;
+
 	if (tree == NULL)
 		return (0);
-	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+
+	left_height = tree->left ? binary_tree_size(tree->left) + 1 : 0;
+	right_height = tree->right ? binary_tree_size(tree->right) + 1 : 0;
+
+	return (left_height > right_height ? left_height : right_height);
 }
