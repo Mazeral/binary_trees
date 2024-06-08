fef25fee3555fc85dbd24e2da66a7cc33e3699b8
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-06-08 03:37:21 +0200

    nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -10,5 +10,5 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		return (1);
 	if (tree == NULL)
 		return (0);
-	return (binary_tree_nodes(tree));
+	return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 }
