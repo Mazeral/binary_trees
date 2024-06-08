598f6bfe1d3859dabe5396631d1d61521adffbd3
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-06-08 03:40:27 +0200

    nodes

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,7 +8,7 @@ size_t binary_tree_size(const binary_tree_t *tree);
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	return (int)(binary_tree_size(tree->left) - binary_tree_size(tree->right));
+	return ((int)(binary_tree_size(tree->left) - binary_tree_size(tree->right)));
 }
 
 /**
